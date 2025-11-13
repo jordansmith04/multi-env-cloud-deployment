d7719904995a21479d4907fcf03b3582641a39cb
Jordan Smith <jordan.smith@peraton.com>
2025-11-13 14:56:18 -0800

    Adding backend for tfstate management

---
+++ terraform/environments/dev/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    # Placeholders - use CI/CD variables for these
+    bucket         = "tf-state-backend-bucket-name" 
+    key            = "dev.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
